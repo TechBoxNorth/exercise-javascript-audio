5af7c7f69eea009af5a9d005c94764d13ef12d0a
TechBoxNorth <techboxnorth@gmail.com>
2024-02-02 18:32:15 +0100

    scrapping bonus features

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const audioPlayer = document.querySelector('.audio-player');
 const libraryList = document.querySelector('.library-list');
-let playList = [];
 // ------- create song library ---------------------
 function createLibraryListItem(i){
     let item = document.createElement('li');
     item.classList.add('library-item');
     item.innerHTML = `<img class="library-art" src="${songObjects[i].cover}" alt="">
     <p>Song: ${songObjects[i].name}</p>
-    <p>Artist: ${songObjects[i].artist}</p><img class="add-to-playlist" src="assets/gui/playlist_add.svg" alt="">`;
-    item.id = songObjects[i].id;
+    <p>Artist: ${songObjects[i].artist}</p>`;
+    item.id = songObjects[i].id - 1;
     libraryList.appendChild(item);
 }
 
@@ -18,16 +17,8 @@ for(let i = 0; i < songObjects.length; i++){
 
 libraryList.addEventListener('click', (e) => {
     const listItem = e.target.closest('.library-item')
-    if(!e.target.classList.contains('add-to-playlist')){
-        const songId = listItem.id;
-        audioPlayer.src = songObjects[songId].song;
-        audioPlayer.play();
-    } 
-    else {
-        playList.push(listItem);
-        console.log(playList);
-    }
+    const songId = listItem.id;
+    audioPlayer.src = songObjects[songId].song;
+    audioPlayer.play();
 });
-
-// --------------------------------------------------
 
