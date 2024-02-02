8bcfb4c8d476f54dfe8b2fe409d0b72f3771d060
TechBoxNorth <techboxnorth@gmail.com>
2024-02-02 14:37:14 +0100

    began pushing song objects to playlist array

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const audioPlayer = document.querySelector('.audio-player');
 const libraryList = document.querySelector('.library-list');
+let playList = [];
 // ------- create song library ---------------------
 function createLibraryListItem(i){
     let item = document.createElement('li');
@@ -16,10 +17,15 @@ for(let i = 0; i < songObjects.length; i++){
 }
 
 libraryList.addEventListener('click', (e) => {
+    const listItem = e.target.closest('.library-item')
     if(!e.target.classList.contains('add-to-playlist')){
-        const songId = e.target.closest('.library-item').id;
+        const songId = listItem.id;
         audioPlayer.src = songObjects[songId].song;
         audioPlayer.play();
+    } 
+    else {
+        playList.push(listItem);
+        console.log(playList);
     }
 });
 
