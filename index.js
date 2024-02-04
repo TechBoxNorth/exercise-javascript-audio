ef14d767b922bba97b033b0a1d30be3c9546fd9c
TechBoxNorth <techboxnorth@gmail.com>
2024-02-04 13:14:13 +0100

    basic styling of player view done

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
+let activeView = 'player';
+
 const audioPlayer = document.querySelector('.audio-player');
 const libraryList = document.querySelector('.library-list');
+const header = document.querySelector('header');
+
+header.addEventListener('click', (e) => {
+    if(e.target.closest('div').classList.contains('to-songs') && activeView === 'player'){
+        showLibrary();
+    } else {
+        if(e.target.closest('div').classList.contains('to-player') && activeView === 'library'){
+            showPlayer();
+        }
+    }
+});
+
+function showPlayer(){
+    activeView = 'player';
+    document.querySelector('.to-songs').style.display = 'inherit';
+    document.querySelector('.player').style.display = 'inherit';
+    document.querySelector('.to-player').style.display = 'none';
+    document.querySelector('.library').style.display = 'none';
+}
+
+function showLibrary(){
+    activeView = 'library';
+    document.querySelector('.to-songs').style.display = 'none';
+    document.querySelector('.player').style.display = 'none';
+    document.querySelector('.to-player').style.display = 'inherit';
+    document.querySelector('.library').style.display = 'inherit';
+}
+
+showPlayer();
+
 // ------- create song library ---------------------
 function createLibraryListItem(i){
     let item = document.createElement('li');
